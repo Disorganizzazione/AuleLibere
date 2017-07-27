bf0d04dc95393a2165cad2f0221c06577555f370
Arianna Masciolini <uzkamascio@gmail.com>
2017-07-27 22:28:52 +0200

    Leggermente migliorate performance nello scorrimento, aggiunto numero piano al layout dei frammenti, aggiunti orari.

--- app/src/main/java/com/example/harisont/aulelibere/Floor.java
+++ app/src/main/java/com/example/harisont/aulelibere/Floor.java
@@ -1,13 +1,8 @@
 package com.example.harisont.aulelibere;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import org.xmlpull.v1.XmlPullParser;
+//import java.time.LocalTime;
 
 /**
  * Created by harisont on 17/07/17.
@@ -20,7 +15,7 @@ public class Floor extends Fragment {
 
     public void showCurrentSituation() {
         for (int i=0; i<roomn; i++) {
-            if (rooms[i].is_free==true)
+            if (rooms[i].is_free /*&& (LocalTime.now().isAfter(LocalTime.parse(rooms[i].opening_time)) && (LocalTime.now().isAfter(LocalTime.parse(rooms[i].closing_time))))*/)
                 rooms[i].green_view.setVisibility(View.VISIBLE);
             else
                 rooms[i].red_view.setVisibility(View.VISIBLE);
--- app/src/main/java/com/example/harisont/aulelibere/Room.java
+++ app/src/main/java/com/example/harisont/aulelibere/Room.java
@@ -1,6 +1,9 @@
 package com.example.harisont.aulelibere;
 
 import android.widget.ImageView;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Date;
 
 /**
  * Created by harisont on 26/07/17.
@@ -12,12 +15,15 @@ public class Room {
     boolean is_free;
     ImageView green_view;
     ImageView red_view;
-
+    String opening_time;
+    String closing_time;
     //aggiungere orario di apertura-chiusura per incrociare i dati con quelli del db. verde=non prenotata AND aperta
 
     public Room(int id, String name) {
         this.id=id;
         this.name=name;
         this.is_free=true;
+        this.opening_time="9:00";
+        this.closing_time="22:00";
     }
 }
